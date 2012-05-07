3b4abeeab95d0df9da3c3a16b8530ec8f6899b2c
Christopher Lemire <christopher.lemire@gmail.com>
2012-05-07 16:00:49 -0500

    Made code more robust

--- ThreadRace.java
+++ ThreadRace.java
@@ -1,12 +1,6 @@
 public class ThreadRace {
   public static void main(String[] args) {
-    Runnable travis = new Runner("Travis");
-    Runnable chris = new Runner("Chris");
-
-    Thread t1 = new Thread(travis);
-    Thread c1 = new Thread(chris);
-
-    t1.start();
-    c1.start();
+    new Thread(new Runner("Travis")).start();
+    new Thread(new Runner("Chris")).start();
   }
 }
